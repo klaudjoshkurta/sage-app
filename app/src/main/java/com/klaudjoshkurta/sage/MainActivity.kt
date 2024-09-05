7a6aa32fa53c5a3cb27b1497c43c29d32629a35b
George Shkurta <g.shkurta@takt.gr>
2024-09-05 13:08:04 +0300

    Initial commit

---
+++ app/src/main/java/com/klaudjoshkurta/sage/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.klaudjoshkurta.sage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.klaudjoshkurta.sage.ui.theme.SageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SageTheme {
+
+            }
+        }
+    }
+}
